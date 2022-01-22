48428b9f70218fc95276e96ba8dc61e22969f394
halldor <halldorr@gmail.com>
2022-01-22 16:40:10 +0000

    Change directory structure of mppt value storage output

--- src/storeMpptValues.ts
+++ src/storeMpptValues.ts
@@ -1,14 +1,21 @@
 import { existsSync, mkdirSync, writeFileSync } from 'fs';
 
 let lastReport = 0;
-const dir = '../output';
+const baseDir = '../output';
 
 export function storeMpptValues(dataPoint: { [key: string]: any }) {
   if (lastReport++ === 0) {
+    if (!existsSync(baseDir)) {
+      mkdirSync(baseDir);
+    }
+    const time = new Date().toISOString();
+    const date = time.slice(0, 10);
+
+    const dir = `${baseDir}/${date}`;
     if (!existsSync(dir)) {
       mkdirSync(dir);
     }
-    const time = new Date().toISOString();
+
     const dataWithTime = { ...dataPoint, time };
     writeFileSync(`${dir}/${time}.json`, JSON.stringify(dataWithTime));
   }
